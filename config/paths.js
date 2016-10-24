eb9725643ed7257eb980639df0a4f5dfc777c7f4
Arturo Guzman <arturo@guzart.com>
2016-10-24 03:29:41 -0400

    fix(eslint-config): fix using fetching current working directory used for eslint configuration
    Atom uses PWD to defined the project working directory

--- config/paths.js
+++ config/paths.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
-const appDirectory = fs.realpathSync(process.cwd());
+const appDirectory = fs.realpathSync(process.env.PWD || process.cwd());
 function resolveApp(relativePath) {
   return path.resolve(appDirectory, relativePath);
 }
